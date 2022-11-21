94f25ad17e63e9b0b3cdefbfd5286bdc3e2c0449
Johny <rahul.johny@zipstack.com>
2022-11-21 12:47:17 +0530

    Enabling console and swagger API only for admin

--- src/main/java/com/ff4j/ff4japp/config/SecurityConfig.java
+++ src/main/java/com/ff4j/ff4japp/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.ff4j.ff4japp.config;
 
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,29 +11,33 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	
-	/** {@inheritDoc} */
-	@Override
+
+    /** {@inheritDoc} */
+    @Override
     protected void configure(AuthenticationManagerBuilder auth)
-    throws Exception {
+            throws Exception {
         PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
         auth.inMemoryAuthentication()
-            .withUser("user").password(encoder.encode("user"))
-                             .authorities("user").and()
-            .withUser("superuser").password(encoder.encode("superuser"))
-                             .authorities("user", "superuser").and()
-            .withUser("admin").password(encoder.encode("admin"))
-                             .authorities("user", "admin");
+                .withUser("user").password(encoder.encode("user"))
+                .authorities("user").and()
+                .withUser("superuser").password(encoder.encode("superuser"))
+                .authorities("user", "superuser").and()
+                .withUser("admin").password(encoder.encode("admin"))
+                .authorities("user", "admin");
     }
 
     /** {@inheritDoc} */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
-            .authorizeRequests()
-            .anyRequest().authenticated()
-            .and().httpBasic();
-    
+                .authorizeRequests()
+                .antMatchers("/ff4j-web-console/**").hasAuthority("admin")
+                .antMatchers("/api/ff4j/**").hasAuthority("admin")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin();
+        // .httpBasic();
+
     }
 
 }
