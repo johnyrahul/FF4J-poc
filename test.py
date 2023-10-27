2adf5eec0ca933aba56a6d3ff4123ef39ac4b5f1
Rahul Johny <116638720+johnyrahul@users.noreply.github.com>
2023-10-27 19:25:47 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,20 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder \
+    .appName("PostgreSQLConnection") \
+    .getOrCreate()
+jdbc_url = "jdbc:postgresql://mesh-postgres-demo.ce50yhobtvnj.ap-south-1.rds.amazonaws.com:5432/postgres"
+connection_properties = {
+    "user": "postgres",
+    "password": "AjJVTp&wF6a0^3",
+    "driver": "org.postgresql.Driver"  # Path to the PostgreSQL JDBC driver JAR
+}
+
+table_name = "tracks"
+
+df = spark.read \
+    .jdbc(url=jdbc_url, table=table_name, properties=connection_properties)
+
+df.show()
+
+spark.stop()
